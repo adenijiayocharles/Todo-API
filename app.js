d776896507fd4e77130d9a5031dcfee248752286
Adeniji Ayo Charles <adenijiayocharles@gmail.com>
2018-12-27 22:07:40 +0100

    linked static files directory

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
 
 const todoRoutes = require("./routes/todo");
 app.use("/api/todos", todoRoutes);
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html");
+})
 
 module.exports = app;

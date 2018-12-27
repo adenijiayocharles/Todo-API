98ac11bd7da9d8db9286c2ffb8ff87a3b37ceca3
Adeniji Ayo Charles <adenijiayocharles@gmail.com>
2018-12-27 20:54:21 +0100

    switched from mongodb atlas to mlab

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set('debug', true);
 // mongoose.connect("mongodb://localhost/todo-api", { useNewUrlParser: true, useCreateIndex: true });
-mongoose.connect("mongodb+srv://charles:todo@todo-li9rc.mongodb.net/test?retryWrites=true", { useNewUrlParser: true, useCreateIndex: true });
+//mongoose.connect("mongodb+srv://charles:todo@todo-li9rc.mongodb.net/test?retryWrites=true", { useNewUrlParser: true, useCreateIndex: true });
+
+mongoose.connect("mongodb://todo:charles1@ds145184.mlab.com:45184/todosapi", { useNewUrlParser: true, useCreateIndex: true });

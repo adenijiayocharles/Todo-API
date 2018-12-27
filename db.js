39b6e3de4c90d7486ba8bcd61ba3557354d049a9
Adeniji Ayo Charles <adenijiayocharles@gmail.com>
2018-12-27 17:35:19 +0100

    switched database from local to mongodb atlas

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 const mongoose = require("mongoose");
 mongoose.set('debug', true);
-mongoose.connect("mongodb://localhost/todo-api", { useNewUrlParser: true, useCreateIndex: true });
+// mongoose.connect("mongodb://localhost/todo-api", { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect("mongodb+srv://charles:todo@todo-li9rc.mongodb.net/test?retryWrites=true", { useNewUrlParser: true, useCreateIndex: true });

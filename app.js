976cd239e91113491ca38b8474a5a035b0a7d169
Adeniji Ayo Charles <adenijiayocharles@gmail.com>
2018-12-28 06:41:16 +0100

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const app = express();
 const db = require("./db");
 const bodyParser = require("body-parser");
 
+// cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
-// window.addEventListener("load", function (){
-//     let xhr = new XMLHttpRequest();
-//     xhr.get("GET", "")
-// });
+window.addEventListener("load", function (){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://restfulltodo.herokuapp.com/api/todos", true);
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            console.log(xhr.responseText);
+        }
+    }
+    xhr.send();
+});

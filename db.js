5484eb9a3a3de8db8f5b061124211a5d2389cf89
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2019-09-22 18:34:50 +0100

    fix audit issue

--- db.js
+++ db.js
@@ -3,4 +3,4 @@ mongoose.set('debug', true);
 // mongoose.connect("mongodb://localhost/todo-api", { useNewUrlParser: true, useCreateIndex: true });
 //mongoose.connect("mongodb+srv://charles:todo@todo-li9rc.mongodb.net/test?retryWrites=true", { useNewUrlParser: true, useCreateIndex: true });
 
-mongoose.connect("mongodb://todo:charles1@ds145184.mlab.com:45184/todosapi", { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect("mongodb://todo:charles1@ds145184.mlab.com:45184/todosapi", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
